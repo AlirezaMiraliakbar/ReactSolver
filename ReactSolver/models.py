224a0d949cf119daae2fcaa5d57e48d5a9a53948
Alireza Miraliakbar <ar.miraliakbar@gmail.com>
2024-10-25 08:04:48 -0400

    cstr

--- ReactSolver/models.py
+++ ReactSolver/models.py
@@ -9,21 +9,47 @@ class CSTR:
         self.phase = params['phase']
         self.reaction = reaction
         self.feed = params['feed']
+
     def solve_ss(self):
         # solving for the mole balances of species involved in the reaction
         sol_guess = np.ones(self.reaction.n_species)
         sol = fsolve(self.mole_balance, sol_guess)
     # def solve_transient(self):
-    def mole_balance(self,x):
-        # x(1) = V
-        # x(2) = C_A
-        
+
+    def mole_balance_ss(self,x):
+        # x[0:n_reactants] = F[reactant]
+        # x[-1] = V
+        Ft = np.sum(x[:-1])
         y = []
+        C = np.zeros(self.reaction.n_reactants)
+        P = self.calc_pressure(T, Ft)
+        T = self.energy_balance(x)
+        ri = np.zeros(self.reaction.n_reactants)
+        # multiple reaction shows itself in the rate 
         for i in range(self.reaction.n_reactants):
-            eqn = self.feed[i] - x + self.reaction.r[i]*
-            y.append()
+            vi = self.calc_vol_rate(T, P, Ft)
+            C[i] = x[i]/self.calc_vol_rate(T, P, Ft)
+            ri[i] = vi*self.reaction.calc_rate(i,C[i],T)
+            eqn = self.feed[i] - x[i] + ri[i]*x[-1]
+            y.append(eqn)
 
         return y
+    
+    def energy_balance(self,x):
+        return self.T0
+    
+    def calc_vol_rate(self, T, P, Ft):
+        # F is the flowrate
+        # T is the temperature
+        # P is the pressure
+        if self.phase == 'liquid':
+            self.nu = self.nu0
+        elif self.phase == 'gas':
+            self.nu = self.nu0 * (T/self.T0) * (self.P0/P)
+        return self.nu
+    
+    def calc_pressure(self, T, Ft):
+        return self.P0
 
     def plot(self, sol):
         plt.plot(sol.t, sol.y.T)
--- ReactSolver/reaction.py
+++ ReactSolver/reaction.py
@@ -5,19 +5,20 @@ class SingleReaction:
         self.n_reactants = params['n_reactants']
         self.n_products = params['n_products']
         self.n_species = self.n_products + self.n_reactants
-        self.stoich_matrix = params['stoich_matrix']
+        self.stoich_vec = params['stoich_vec']
         self.order = params['order']
-        self.k = params['k']
+        # self.k = params['k']
         self.reversible = params['reversible']
         self.Ea = params['Ea']
         self.A = params['A']
     # the reaction rate needs to be upadated coupled with the reactor model
         self.rate = np.zeros([self.n_reactants,1])
-    def calc_rate(self, index,C):
+    def calc_rate(self, index,C, T):
         # C is the concentration of the reactants
+        self.k = self.A * np.exp(-self.Ea/(self.R*T))
         self.rate[index] = self.stoich_matrix[index] * self.k * (C**self.order)
         return self.rate
-
+#TODO: Units issue must be resolved
 class MultiReaction:
     def __init__(self,):
         self.reactions = []
